542db1f37b33007c8aa3e9ed3bf54536315c540a
Aby Kal <abyabrkal@gmail.com>
2020-03-06 17:44:50 +0400

    header structure

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,12 @@ import React from 'react'
 const Header = () => {
     return (
         <div>
-            <h1>Aby Kal</h1>
+            <h1>Hello, I'm Aby Kal</h1>
+            <p>I’m an IT Administrator currently managing IT and software operations of Sarsan Group of companies at Dubai. 
+            I have experience as Senior Software Engineer in Aerospace domain and has lead development and verification activities of various military and commercial aircraft projects. 
+            I develop and maintain company websites and experiment on React, Node, AWS, Dockers and Kubernetes. 
+            I can't shut up about test-driven development and utility-first CSS. 
+            I started sharing what I learn on <a href="blog.abykal.me">my blog</a>.</p>
         </div>
     )
 }
