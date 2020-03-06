0aa6c7d200dfa30d64e1f6afc7f255874e793498
Aby Kal <abyabrkal@gmail.com>
2020-03-06 17:21:13 +0400

    TailwindCSS added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
+import Header from './components/Header'
+import Contacts from './components/Contacts'
+import Projects from './components/Projects'
+import Blogs from './components/Blogs'
+import Footer from './components/Footer'
+
 function App() {
   return (
     <div className="">
