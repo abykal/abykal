34e4359e0e59813bd947caa7284e7f20d195b07e
Aby Kal <abyabrkal@gmail.com>
2020-03-11 10:45:11 +0400

    minor styling errors removed

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,14 +3,13 @@ import React from "react";
 const Projects = () => {
   return (
     <div className="md:pt-6 md:-mt-7">
-      <h2 className="top-0 z-40 pt-6 my-6 text-sm font-bold tracking-widest uppercase text-white">
+      <h2 className="top-0 z-40 pt-6 my-10 text-sm font-bold tracking-widest uppercase text-white">
         Projects
       </h2>
       <div className="flex flex-wrap -my-4 md:-m-4 group pt-8 pb-9 md:py-0" >
         {[1, 2, 3, 4].map((index) => (
           <article
-            className="flex-none w-full my-1 md:m-1 md:w-240 md:min-h-240 lg:w-320 lg:min-h-320 bg-dusk opacity-50 odd:-translate-y-12"
-             style="transform: translateY(-25.4px);"
+            className="flex-none w-full my-1 md:m-1 md:w-240 md:min-h-240 lg:w-320 lg:min-h-320 bg-dusk opacity-50 odd:-translate-y-12 art-card"
             key="index"
           >
             <div className="flex flex-col justify-between w-full h-full p-3 min-h-inherit md:group-hover:opacity-50 md:hover:scale-11/10x md:hover:opacity-important transition">
