37abc44f4588cac42b238963eb986cb767e14580
Aby Kal <abyabrkal@gmail.com>
2020-03-11 19:39:39 +0400

    function

--- src/components/Greetings.js
+++ src/components/Greetings.js
@@ -3,10 +3,9 @@ import { dailyGreet, splGreet } from '../helpers/getgreet'
 
 const Greetings = () => {
     return (
-        <div className="h-6 m-6 flex flex-row justify-between text-blue-300">
-            <p className="">{dailyGreet}</p>
-            <h3 className="">{splGreet}</h3>
-            <p className="mx-10 pr-8">Dubai</p>
+        <div className="h-6 m-6 lg:pl-20 flex flex-row justify-between text-blue-300">
+            <h3 className="">{splGreet()}</h3>
+            <p className="mx-10 pr-8">{dailyGreet()}, Dubai</p>
         </div>
     )
 }
